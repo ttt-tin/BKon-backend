626a3bbc99375a891b539f82f7bb7c4f1a8b6a18
trongtin17 <tin.tranthanh@hcmut.edu.vn>
2024-04-21 21:33:05 +0700

    Backend deploy

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BKon backend API document')
+    .setDescription('The BKon API description')
+    .setVersion('1.0')
+    .addTag('BKon')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
