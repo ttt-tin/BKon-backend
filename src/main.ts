119c28ada126f1efb3baea34fca87d27fa2d19f2
trongtin17 <tin.tranthanh@hcmut.edu.vn>
2024-04-21 22:17:30 +0700

    Fix vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,17 @@ async function bootstrap() {
     .addTag('BKon')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
